f7b32b120fdf95a849d87ab0cbc858ebe6373402
Erin Dachtler <download333@gmail.com>
2016-10-01 20:21:26 -0700

    main db named correctly

--- src/server/config.js
+++ src/server/config.js
@@ -1,6 +1,6 @@
 export default {
 	protocol: 'https',
 	domain: 'grimterest-download13.c9users.io',
-	database: 'storage/main',
+	database: 'storage/main.db',
 	sessionDatabase: 'storage/session'
 };
